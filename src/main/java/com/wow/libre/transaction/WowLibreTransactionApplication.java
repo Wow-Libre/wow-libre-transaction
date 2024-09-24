c4d463bec50142b42d3edf338d13f79ffab1de59
Sebastian Chitiva <mschitiva68@ucatolica.edu.co>
2024-09-23 22:57:07 -0500

    initial commit

---
+++ src/main/java/com/wow/libre/transaction/WowLibreTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.wow.libre.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowLibreTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WowLibreTransactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wow/libre/transaction/WowLibreTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wow.libre.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowLibreTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
