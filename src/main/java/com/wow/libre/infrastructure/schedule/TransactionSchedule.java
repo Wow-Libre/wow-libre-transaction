84581b7def2a12e2bfc60b8ef19bcacd8acc9342
Sebastian Chitiva <mschitiva68@ucatolica.edu.co>
2024-11-30 02:02:53 -0500

    le bajo tiempo a el envio de items

--- src/main/java/com/wow/libre/infrastructure/schedule/TransactionSchedule.java
+++ src/main/java/com/wow/libre/infrastructure/schedule/TransactionSchedule.java
@@ -30,7 +30,7 @@ public class TransactionSchedule {
     }
 
     @Transactional
-    @Scheduled(cron = "* */40 * * * *")
+    @Scheduled(cron = "1/50 * * * * *")
     public void sendPurchases() {
         final String transactionId = "";
         List<TransactionEntity> transactionEntities = obtainTransaction.findByStatusIsPaidAndSendIsFalse(transactionId);
