ee9885e4fcfc333216a04ce22df4af0de1b7ad0d
Sebastian Chitiva <manuchitivar@gmail.com>
2024-12-30 16:17:36 -0500

    agrego logs

--- src/main/java/com/wow/libre/infrastructure/schedule/TransactionSchedule.java
+++ src/main/java/com/wow/libre/infrastructure/schedule/TransactionSchedule.java
@@ -7,6 +7,7 @@ import com.wow.libre.domain.port.in.wowlibre.*;
 import com.wow.libre.domain.port.out.transaction.*;
 import com.wow.libre.infrastructure.entities.*;
 import jakarta.transaction.*;
+import org.slf4j.*;
 import org.springframework.scheduling.annotation.*;
 import org.springframework.stereotype.*;
 
@@ -14,6 +15,7 @@ import java.util.*;
 
 @Service
 public class TransactionSchedule {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TransactionSchedule.class);
 
     private final ObtainTransaction obtainTransaction;
     private final SaveTransaction saveTransaction;
@@ -30,23 +32,27 @@ public class TransactionSchedule {
     }
 
     @Transactional
-    @Scheduled(cron = "1/50 * * * * *")
+    @Scheduled(cron = "1/20 * * * * *")
     public void sendPurchases() {
         final String transactionId = "";
         List<TransactionEntity> transactionEntities = obtainTransaction.findByStatusIsPaidAndSendIsFalse(transactionId);
 
-        for (TransactionEntity transaction : transactionEntities) {
-            final String jwt = wowLibrePort.getJwt(transactionId);
+        try {
+            for (TransactionEntity transaction : transactionEntities) {
+                final String jwt = wowLibrePort.getJwt(transactionId);
 
-            List<ItemQuantityModel> items = packagesPort.findByProductId(transaction.getProductId(), transactionId);
-            Double amount = transaction.isGold() ? transaction.getPrice() : 0d;
-            wowLibrePort.sendPurchases(jwt, transaction.getServerId(), transaction.getUserId(),
-                    transaction.getAccountId(), amount, items, transaction.getReferenceNumber(), transactionId);
-            transaction.setSend(true);
-            if (transaction.isGold()) {
-                transaction.setStatus(TransactionStatus.DELIVERED.getType());
+                List<ItemQuantityModel> items = packagesPort.findByProductId(transaction.getProductId(), transactionId);
+                Double amount = transaction.isGold() ? transaction.getPrice() : 0d;
+                wowLibrePort.sendPurchases(jwt, transaction.getServerId(), transaction.getUserId(),
+                        transaction.getAccountId(), amount, items, transaction.getReferenceNumber(), transactionId);
+                transaction.setSend(true);
+                if (transaction.isGold()) {
+                    transaction.setStatus(TransactionStatus.DELIVERED.getType());
+                }
+                saveTransaction.save(transaction, transactionId);
             }
-            saveTransaction.save(transaction, transactionId);
+        } catch (Exception e) {
+            LOGGER.error("Error Transaction Sends {}", e.getLocalizedMessage());
         }
 
     }
